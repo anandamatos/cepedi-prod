cf55f475fbea638d9577e0deee6eaf834bd4284a
Ananda Matos <anandamatos@gmail.com>
2025-11-09 23:37:16 -0300

    organiza ambiente de prod + test fazer em sala de aula

---
+++ tasks/aula16/ex02/script.js
@@ -0,0 +1,22 @@
+/*
+
+*/
+
+class Retangulo{
+    constructor(nome, largura, altura){
+        this.nome = nome;
+        this.largura = largura;
+        this.altura = altura;
+    }
+    ObterNome() {
+        return this.nome;
+    }
+        calcularPerimetro() { 
+            return 2 * (this.largura + this.altura);
+        }
+        calculaArea() {
+            return this.largura * this.altura;
+        }
+        calcula.
+    }
+}
