05333083eb11ad2e680e878143817b87c47bc270
Ananda Matos <anandamatos@gmail.com>
2026-03-21 13:16:57 -0300

    fix: remove unused useEffect to fix build

--- mod5_react-task/src/App.js
+++ mod5_react-task/src/App.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import UserCard from './components/UserCard';
 import ProjectList from './components/ProjectList';
